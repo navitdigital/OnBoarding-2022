a2b7dd17b125af6feead463643d8ce6768182d1e
Geovanne Oliveira <geovanneoliveira3@gmail.com>
2022-03-24 11:06:11 -0300

    :tada: VUE

---
+++ VUE/src/App.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <Listinha :lista="listaDeContatos"/>
+    <p :class="getColor">
+      {{ getMessage }}
+    </p>
+    <p v-if="status">STATUS É TRUE</p>
+    <p v-else>STATUS É FALSE</p>
+  </div>
+</template>
+
+<style scoped>
+.text {
+  color: black;
+}
+.text.active {
+  color: red;
+}
+</style>
+
+<script>
+
+import Listinha from '@/components/Listinha/Listinha.vue'
+
+export default {
+  name: "Home",
+  components: {
+    Listinha
+  },
+  data() {
+    return {
+      message: "Navit",
+      status: true,
+      listaDeContatos: [
+        {
+          active: false,
+          name: "Dayvid",
+        },
+        {
+          active: false,
+          name: "Wesso",
+        },
+        {
+          active: false,
+          name: "Jorge",
+        },
+        {
+          active: false,
+          name: "teste",
+        },
+      ],
+    };
+  },
+  computed: {
+    getMessage() {
+      if (this.listaDeContatos.every((e) => e.active)) {
+        return "Todo mundo selecionado";
+      } else {
+        return "Nem todo mundo foi selecionado";
+      }
+    },
+    getColor() {
+      return this.listaDeContatos.every((e) => e.active) ? "text active" : "text";
+    },
+  },
+  methods: {
+    handleClick(id) {
+      this.listaDeContatos[
+        id
+      ].name = `${this.listaDeContatos[id].name} - clicado`;
+
+      this.listaDeContatos[id].active = !this.listaDeContatos[id].active;
+    },
+  },
+  created() {
+   this.handleClick(0)
+  },
+  mounted() {
+    this.listaDeContatos[0].active = false
+  },
+};
+</script>
---
+++ VUE/src/components/Listinha/Listinha.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(bla, index) in lista" :key="index">
+        <div @click="handleClick(index)" style="padding: 10px; cursor: pointer">
+          <span :class="bla.active ? `text active` : `text`">
+            {{ bla.name }}
+          </span>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.text {
+  color: black;
+}
+.text.active {
+  color: red;
+}
+</style>
+
+<script>
+export default {
+  name: "Listinha",
+  data() {
+    return {
+      message: "Navit",
+      status: true,
+    };
+  },
+  props: {
+    lista: Array
+  },
+  computed: {
+    getMessage() {
+      if (this.lista.every((e) => e.active)) {
+        return "Todo mundo selecionado";
+      } else {
+        return "Nem todo mundo foi selecionado";
+      }
+    },
+    getColor() {
+      return this.lista.every((e) => e.active)
+        ? "text active"
+        : "text";
+    },
+  },
+  methods: {
+    handleClick(id) {
+      this.lista[
+        id
+      ].name = `${this.lista[id].name} - clicado`;
+
+      this.lista[id].active = !this.lista[id].active;
+    },
+  },
+  created() {
+    this.lista[0].active = true;
+  },
+  mounted() {
+    this.lista[0].active = false;
+  },
+};
+</script>
